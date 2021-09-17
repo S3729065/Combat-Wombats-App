fdaa806156fb6fb2e2d699a689ad772dfbf28582
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-09-17 16:10:28 +1000

    replaced old files with new files

---
+++ Wombat/src/main/java/combat/wombats/Wombat/Data/UserDbo.java
@@ -0,0 +1,6 @@
+package combat.wombats.Wombat.Data;
+
+import combat.wombats.Wombat.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserDbo extends JpaRepository<User, Integer> {}
---
+++ Wombat/src/test/java/combat/wombats/Wombat/WombatApplicationTests.java
@@ -0,0 +1,13 @@
+package combat.wombats.Wombat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WombatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

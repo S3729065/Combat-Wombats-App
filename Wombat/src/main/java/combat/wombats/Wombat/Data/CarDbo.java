16fef8fdbb68d33ea3598d2540c803c992020d13
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-09-17 16:09:07 +1000

    removed files

--- Wombat/src/main/java/combat/wombats/Wombat/Data/CarDbo.java
+++
@@ -1,6 +0,0 @@
-package combat.wombats.Wombat.Data;
-
-import combat.wombats.Wombat.Models.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarDbo extends JpaRepository<Car, Integer> {}
--- Wombat/src/main/java/combat/wombats/Wombat/Data/UserDbo.java
+++
@@ -1,6 +0,0 @@
-package combat.wombats.Wombat.Data;
-
-import combat.wombats.Wombat.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDbo extends JpaRepository<User, Integer> {}
